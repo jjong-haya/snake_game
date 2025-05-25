8e5cd7eccf4965326a36f97f06863e5f08adb936
jjong-haya <jongha0315@gmail.com>
2025-05-25 17:08:45 +0900

    Add files via upload
    step1 맵 출력 및 여러 시각적 표지 제작 ex) intro

---
+++ step 1/src/core/map.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#define MAP_SIZE 21
+
+using namespace std;
+
+class MapManager {
+private:
+  int stage;
+  vector<vector<int>> mapData;
+
+public:
+  MapManager();
+  void setStage(int s);
+  void load(const string &filename);
+  void print() const;
+  void printStageIntro();
+  void printNextStage();
+  void printIntro();
+  int getStage() const;
+  bool isEmpty(int y, int x) const;
+  int getCell(int y, int x) const;
+  void setCell(int y, int x, int value);
+};
